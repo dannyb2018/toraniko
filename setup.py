7a06c4960474f9a62f9ad28f74fb9afb0907e040
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2025-06-14 13:46:58 +0900

    use dev requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = []
-with open('requirements.txt', 'r') as fh:
+with open('dev-requirements.txt', 'r') as fh:
     for line in fh:
         requirements.append(line.strip())
 
