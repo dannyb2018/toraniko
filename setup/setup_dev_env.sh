5bf4821308b18d02f7e954680b5ca0c5f078de7a
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2025-06-14 13:47:22 +0900

    removes old installs, cleans and installs new version

--- setup/setup_dev_env.sh
+++ setup/setup_dev_env.sh
@@ -1,4 +1,10 @@
+rm -rf ../.build
+rm -rf ../dist
+rm -rf ../toraniko.egg-info
 rm -rf ../.venv
 python3 -m venv ../.venv
 source ../.venv/bin/activate
 pip install -r ../dev-requirements.txt
+cd ..
+python3 ./setup.py clean
+python3 ./setup.py install
