636df3fb5e36f00f75b338b3959ed0a757a5d794
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2025-09-07 17:34:27 +0900

    need to install setuptools

--- setup/setup_dev_env.sh
+++ setup/setup_dev_env.sh
@@ -4,6 +4,7 @@ rm -rf ../toraniko.egg-info
 rm -rf ../.venv
 python3 -m venv ../.venv
 source ../.venv/bin/activate
+pip install setuptools
 pip install -r ../dev-requirements.txt
 cd ..
 python3 ./setup.py clean
